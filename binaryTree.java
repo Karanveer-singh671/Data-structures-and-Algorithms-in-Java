79161272e7be0aaea21fb4cc80861eb918481c00
Karanveer Singh <singhkaranveer671@gmail.com>
2019-04-09 19:47:15 -0700

    create binary tree

---
+++ binaryTree.java
@@ -0,0 +1,9 @@
+// non linear data structure used for storing data
+// made up of nodes and edges without having any cycle 
+// each node in a tree can point to n number of nodes in a tree
+// parent node called root and many levels of additional nodes
+// node with no children (pointing to any other nodes) are called leaf nodes.
+
+/* Binary tree if each node has 0, 1 or 2 children
+TreeNode is: 
+null<--|left|data|right --> null 
