fee34aa0185cf23c8364d19b94b080cda3c924dc
Karanveer Singh <singhkaranveer671@gmail.com>
2019-04-09 22:39:43 -0700

    BST insertion recursive

--- binarySearchTree.java
+++ binarySearchTree.java
@@ -10,17 +10,30 @@ null <-- left|data|right --> null
 public class BinarySearchTree {
   // instance var property of BST
   private TreeNode root;
-
+  // inner class
     private class TreeNode {
       // property of TreeNode
       private TreeNode left;
       private TreeNode right;
       private int data;
-
+      // constructor in class to be called on creation 
       public TreeNode(int data) {
         this.data = data
       }
     }
+
+  public TreeNode insert(TreeNode root, int value) {
+    if(root == null) {
+      root = new TreeNode(value)
+      return root
+    }
+    if(value < root.data) {
+      root.left = insert(root.left, value)
+    } else {
+      root.right = insert(root.right, value)
+    }
+    return root
+  }
 
 
   public static void main(String[], args) {
